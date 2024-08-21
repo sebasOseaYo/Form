6fb4344f6de64fb95fc9125c1adae9f42fb321b5
mayasavant <Juan.maya@savant-bpo.com>
2024-08-21 10:17:19 -0500

    New structure

--- drivers-form/src/components/Form/form.jsx
+++ drivers-form/src/components/Form/form.jsx
@@ -1,66 +1,129 @@
 import React from 'react'
 import './form.css'
-import { Tooltip } from '@mui/material'
+// MATERIAL UI
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
+import InputLabel from '@mui/material/InputLabel';
+import Input from '@mui/material/Input';
+import InputAdornment from '@mui/material/InputAdornment';
+import Button from '@mui/material/Button';
+import { styled } from '@mui/material/styles';
+// MATERIAL UI ICONS
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import NumbersIcon from '@mui/icons-material/Numbers';
+import EmailIcon from '@mui/icons-material/Email';
+import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
+import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+
+
+const VisuallyHiddenInput = styled('input')({
+    clip: 'rect(0 0 0 0)',
+    clipPath: 'inset(50%)',
+    height: 1,
+    overflow: 'hidden',
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    whiteSpace: 'nowrap',
+    width: 1,
+});
 
 const form = () => {
     return (
         <>
-            <div className="page-container">
-                <div className="form-container">
-                    {/* ELECCIÓN DE FORMULARIO */}
-                    <div className="form-checker">
-                        <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1" />
-                            <label class="form-check-label" for="inlineRadio1">BOL</label>
-                        </div>
-                        <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2" />
-                            <label class="form-check-label" for="inlineRadio2">POD</label>
-                        </div>
-                        <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio3" value="option3" />
-                            <label class="form-check-label" for="inlineRadio3">J1</label>
-                        </div>
-                        <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio3" value="option3" />
-                            <label class="form-check-label" for="inlineRadio3">Gas Receips</label>
-                        </div>
-                    </div>
-                    <form>
-                        {/* UTILIDADES */}
-                        <div className="utilities">
-                            <Tooltip title="Remove Form">
-                                <i className="fa-solid fa-xmark"></i>
-                            </Tooltip>
-                            <Tooltip title="Add Form">
-                                <i className="fa-solid fa-plus"></i>
-                            </Tooltip>
-                        </div>
-                        {/* FORMULARIO */}
-                        <h1 className="title">Name Form</h1>
-                        <div className="mb-3">
-                            <la className="form-label">4 Driver</la>
-                            <input type="fordriver" required className="form-control" />
-                        </div>
-                        <div className="mb-3">
-                            <label className="form-label">Name</label>
-                            <input type="name" required className="form-control" />
-                        </div>
-                        <div className="mb-3">
-                            <label className="form-label">Email</label>
-                            <input type="email" required className="form-control" />
-                        </div>
-                        <div className="mb-3">
-                            <label className="form-label">Phone Number</label>
-                            <input type="number" required className="form-control" />
-                        </div>
-                        <label className="form-label">File</label>
-                        <div class="input-group mb-3">
-                            <input type="file" required class="form-control" />
-                        </div>
-                    </form>
-                </div>
+            <div className="form-container">
+                <form>
+                    <FormControl>
+                        <FormLabel id="demo-row-radio-buttons-group-label">Type</FormLabel>
+                        <RadioGroup
+                            row
+                            aria-labelledby="demo-row-radio-buttons-group-label"
+                            name="row-radio-buttons-group">
+                            <FormControlLabel value="BOL" control={<Radio />} label="BOL" />
+                            <FormControlLabel value="POD" control={<Radio />} label="POD" />
+                            <FormControlLabel value="J1" control={<Radio />} label="J1" />
+                            <FormControlLabel value="Gas Receipts" control={<Radio />} label="Gate Receipts" />
+                        </RadioGroup>
+                    </FormControl>
+                    <InputLabel htmlFor="input-with-icon-adornment">
+                        4 Driver
+                    </InputLabel>
+                    <Input sx={{
+                        width: '100%',
+                        marginBottom: 2
+                    }}
+                        id="input-with-icon-adornment"
+                        startAdornment={
+                            <InputAdornment position="start">
+                                <NumbersIcon />
+                            </InputAdornment>
+                        }
+                    />
+                    <InputLabel htmlFor="input-with-icon-adornment">
+                        Name
+                    </InputLabel>
+                    <Input sx={{
+                        width: '100%',
+                        marginBottom: 2
+                    }}
+                        type='name'
+                        id="input-with-icon-adornment"
+                        startAdornment={
+                            <InputAdornment position="start">
+                                <AccountCircleIcon />
+                            </InputAdornment>
+                        }
+                    />
+                    <InputLabel htmlFor="input-with-icon-adornment">
+                        Email
+                    </InputLabel>
+                    <Input sx={{
+                        width: '100%',
+                        marginBottom: 2
+                    }}
+                        type='email'
+                        id="input-with-icon-adornment"
+                        startAdornment={
+                            <InputAdornment position="start">
+                                <EmailIcon />
+                            </InputAdornment>
+                        }
+                    />
+                    <InputLabel htmlFor="input-with-icon-adornment">
+                        Phone
+                    </InputLabel>
+                    <Input sx={{
+                        width: '100%'
+                    }}
+                        type='number'
+                        id="input-with-icon-adornment"
+                        startAdornment={
+                            <InputAdornment position="start">
+                                <LocalPhoneIcon />
+                            </InputAdornment>
+                        }
+                    />
+                    <Button sx={{
+                        marginTop: 3,
+                        width: '100%'
+                    }}
+                        component="label"
+                        role={undefined}
+                        variant="contained"
+                        tabIndex={-1}
+                        startIcon={<CloudUploadIcon />}
+                    >
+                        Upload file
+                        <VisuallyHiddenInput type="file" />
+                    </Button>
+
+                </form>
             </div>
+
+
         </>
     )
 }
--- drivers-form/src/pages/Forms/forms.jsx
+++ drivers-form/src/pages/Forms/forms.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import Form from '../../components/Form/form'
 import './forms.css'
+import Button from '@mui/material/Button';
+import SendIcon from '@mui/icons-material/Send';
 
 const forms = () => {
     return (
         <>
             <div className="form-render">
-                <Form />
-                <button type="submit" className="btn btn-primary">Submit</button>
+                <Form className="form" />
+                <Button variant="contained" endIcon={<SendIcon />}>
+                    Send
+                </Button>
             </div>
         </>
     )
